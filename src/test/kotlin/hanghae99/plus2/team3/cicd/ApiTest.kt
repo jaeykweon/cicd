ef8caae1ad5984c752f42c1a81e764fcb67f037c
jay <jaeykweon@naver.com>
2023-09-14 23:22:33 +0900

    workflow environment 설정

--- src/test/kotlin/hanghae99/plus2/team3/cicd/ApiTest.kt
+++ src/test/kotlin/hanghae99/plus2/team3/cicd/ApiTest.kt
@@ -5,9 +5,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.server.LocalServerPort
-import org.springframework.test.context.ActiveProfiles
 
-@ActiveProfiles("alpha")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApiTest {
 
